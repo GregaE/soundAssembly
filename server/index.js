55a2e27ce617d9124c09508c20294c1b8af23de4
gregae <grega.erzen@outlook.com>
2022-01-17 16:52:26 +0100

    refactor: cors config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const Express = require('express');
 const router = require('./router');
 const cors = require('cors');
 const db = require('./model/dbaccess.js');
-const corsConfig = {origin: ['http://localhost:3000']};
+const corsConfig = {origin: [process.env.SPOTIFY_REDIRECT_URI]};
 
 const app = Express();
 const PORT = (process.env.PORT || 8889);
